d3b77e107da5d0c6495ea64c00cc8db17d6564a6
vasqc <90007988+vasqc@users.noreply.github.com>
2022-03-06 20:33:35 -0500

    working code on new port

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -13,12 +13,12 @@ const {
 Router.route('/').get(getThought).post(createThought);
 
 // /api/thoughts/:thoughtId
-//router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+Router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
 
 // /api/thoughts/:thoughtId/reactions
-//router.route('/:thoughtId/reactions').post(addReaction);
+Router.route('/:thoughtId/reactions').post(addReaction);
 
 // /api/thoughts/:thoughtId/reactions/:reactionId
-//router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
+Router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
 
 module.exports = Router;
